aab28557b200c1bfc54a0d9bc97bed783bdca08c
NimeshHayleys <nimeshhayleys@gmail.com>
2026-04-14 11:20:38 +0530

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/mkn-portfolio" : "",
+  assetPrefix: isProd ? "/mkn-portfolio/" : "",
 };
 
 export default nextConfig;
